59faefe74b716731e2eb81fdf12d8e5a25adbb78
sariamahmood20@gmail.com <sariamahmood20@gmail.com>
2024-10-07 11:56:17 +0500

    Second Commit

--- lib/app/view_models/user_profile_view_model.dart
+++ lib/app/view_models/user_profile_view_model.dart
@@ -23,10 +23,4 @@ class UserProfileViewModel with ChangeNotifier {
       setUserProfile(ApiResponse.failed(error.toString()));
     });
   }
-
-  @override
-  void dispose() {
-    //
-    super.dispose();
-  }
 }
